a11b40b8188c0b2d0b945ee538023699c9331b5d
null null <20199056@itla.edu.do>
2020-05-25 18:32:49 +0000

    add new file

---
+++ tarea 3/ejercicio5.cs
@@ -0,0 +1,49 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		// Ejercicio #5
+		
+	      
+		  
+		
+		Console.WriteLine("Escriba elperimer numero entero");
+		int num = Convert.ToInt32(Console.ReadLine());
+		
+		
+	
+		
+		if (num == 0)
+		{         
+		            
+		   Console.WriteLine("El producto de 0 por cualquier número es 0");
+			      
+		
+	    }
+		
+	    else if (num>0)
+		{          
+		                 
+			             
+       Console.WriteLine("Escriba el segundo numero entero");
+		  int num2 = Convert.ToInt32(Console.ReadLine());	
+				                        
+      Console.WriteLine("el producto de los dos numeros es: " + num*num2 ); 			
+		
+			
+			                           
+				
+		
+		
+		}
+		
+			
+		
+		
+		
+	}
+}
+
+
